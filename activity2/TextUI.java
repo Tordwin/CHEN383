77fd0f56a1f9cd3d116950db1a0337eb261b172b
Edwin Chen <ec7233@g.rit.edu>
2024-09-02 19:43:50 -0400

    TextUI done

---
+++ activity2/TextUI.java
@@ -0,0 +1,19 @@
+public class TextUI {
+    public static void main(String[] args) {
+        new Thread(() -> {
+            double tempC = 20.0;
+            while (true){
+                tempC += (Math.random() * 2 - 1);
+                double tempK = tempC + 273.15;
+                System.out.println("Reading is " + String.format("%.2f", tempC) 
+                + " degrees C and " + String.format("%.2f", tempK) + " degrees K");
+                try {
+                    Thread.sleep(1000);
+                }
+                catch (InterruptedException ie) {
+                    System.out.println(ie);
+                }
+            }
+        }).start();
+    }
+}
